1ac369746a9836f318c9e5699d1792bb7e636bc2
Manabu Matsuzaki <matsumana@gmail.com>
2012-09-22 00:21:19 +0900

    不要なデバッグログ削除

--- lib/fluent/plugin/in_tail_multiline.rb
+++ lib/fluent/plugin/in_tail_multiline.rb
@@ -3,32 +3,8 @@ module Fluent
 class TailMultiLineInput < TailInput
   Plugin.register_input('tail_multiline', self)
 
-  def initialize
-    super
-
-    #Fluentdのロガーがグローバル変数で提供されている
-    $log.debug 'TailMultiLineInput::initialize'
-  end
-
-  def start
-    super
-
-    #Fluentdのロガーがグローバル変数で提供されている
-    $log.debug 'TailMultiLineInput::start'
-  end
-
-  def shutdown
-    super
-
-    #Fluentdのロガーがグローバル変数で提供されている
-    $log.debug 'TailMultiLineInput::shutdown'
-  end
-
   def configure(conf)
     super
-
-    # config_paramで出来ないような複雑な事も出来る。conf変数に設定ファイルの内容がハッシュで渡ってくる。
-    $log.debug 'TailMultiLineInput::configure ' + conf.to_s
 
     @format = conf['format']
     if @format == nil
@@ -39,8 +15,6 @@ class TailMultiLineInput < TailInput
   end
 
   def receive_lines(lines)
-    $log.debug 'TailMultiLineInput::receive_lines ' + lines.to_s
-
     es = MultiEventStream.new
     record = lines.join
     begin
